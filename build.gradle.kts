7ea1445643ca4134e309e807c515931468b58499
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2021-10-07 19:45:28 +0300

    Groovy-> kotlin scripts

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.android.tools.build:gradle:7.0.2")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+
+    }
+}
+
+plugins{
+    id ("io.gitlab.arturbosch.detekt") version "1.18.1"
+    id ("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+}
+subprojects {
+    apply (plugin = "org.jlleitschuh.gradle.ktlint")
+
+}
+
+dependencies {
+    detektPlugins ("io.gitlab.arturbosch.detekt:detekt-formatting:1.18.1")
+}
+
+detekt {
+    toolVersion = "1.10.0"
+    config = files("config/detekt/detekt.yml")
+    buildUponDefaultConfig = true
+
+    reports {
+        html {
+            enabled = true
+            destination = file("app/build/detekt/detekt.html")
+        }
+    }
+}
+
+task("clean"){
+    doLast{
+        delete(rootProject.buildDir)
+    }
+}
