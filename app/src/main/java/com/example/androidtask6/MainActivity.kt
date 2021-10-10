7cbeec293901834e19452173933513a4eb481238
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2021-10-10 20:32:41 +0300

    delete useless fragment

--- app/src/main/java/com/example/androidtask6/MainActivity.kt
+++ app/src/main/java/com/example/androidtask6/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.androidtask6
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/androidtask6/MainFragment.kt
@@ -0,0 +1,6 @@
+package com.example.androidtask6
+
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+}
