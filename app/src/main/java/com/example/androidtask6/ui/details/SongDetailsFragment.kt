f2f95c78645c4406e49c33fa8f848aa88edfdaf3
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-10-18 14:43:40 +0300

    work with xml's

--- app/src/main/java/com/example/androidtask6/ui/details/SongDetailsFragment.kt
+++ app/src/main/java/com/example/androidtask6/ui/details/SongDetailsFragment.kt
@@ -1,6 +1,37 @@
 package com.example.androidtask6.ui.details
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.androidtask6.databinding.FragmentSongBinding
+import com.example.androidtask6.presentation.MainViewModel
 
 class SongDetailsFragment : Fragment() {
+
+    private var _binding: FragmentSongBinding? = null
+    private val binding: FragmentSongBinding
+        get() = requireNotNull(_binding)
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSongBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- app/src/main/java/com/example/androidtask6/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/androidtask6/ui/home/HomeFragment.kt
@@ -1,6 +1,37 @@
 package com.example.androidtask6.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.androidtask6.databinding.FragmentHomeBinding
+import com.example.androidtask6.presentation.MainViewModel
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = requireNotNull(_binding)
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
