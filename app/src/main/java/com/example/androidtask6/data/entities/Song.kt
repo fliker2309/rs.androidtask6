2f33247dc6235cc28f2372b83e940ab2394053a3
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-10-18 15:52:06 +0300

    add:dataClass Song & playlist.json

---
+++ app/src/main/java/com/example/androidtask6/data/entities/Song.kt
@@ -0,0 +1,9 @@
+package com.example.androidtask6.data.entities
+
+data class Song(
+    val title: String = "",
+    val artist: String = "",
+    val bitmapUri: String = "",
+    val trackUri: String = "",
+    val duration: Long
+)
