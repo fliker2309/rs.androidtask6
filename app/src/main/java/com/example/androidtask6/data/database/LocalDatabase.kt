b44fcdb2c44facc253ed5bfa177eb7767ee3df1a
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-10-21 15:43:13 +0300

    work with service

---
+++ app/src/main/java/com/example/androidtask6/data/database/LocalDatabase.kt
@@ -0,0 +1,10 @@
+package com.example.androidtask6.data.database
+
+import com.example.androidtask6.data.entities.Song
+
+class LocalDatabase {
+
+    fun getAllSongs() : List<Song>{
+
+    }
+}
---
+++ app/src/main/java/com/example/androidtask6/data/json/SongsJson.kt
@@ -0,0 +1,9 @@
+package com.example.androidtask6.data.json
+
+data class SongsJson(
+    val title: String = "",
+    val artist: String = "",
+    val bitmapUri: String = "",
+    val trackUri: String = "",
+    val duration: Long
+)
